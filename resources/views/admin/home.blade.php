ecf7ce5f90e3c59c2714feda3b715b852616cdcb
Dong Huynh <d01295306466@gmail.com>
2021-07-05 11:19:40 +0700

    build layout dashboard and login admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">@lang('auth.title-admin')</h1>
+    </div>
+</div>
+@endsection
