20da7ddd3bde8547069326c5e4e51f91874882df
Dong Huynh <d01295306466@gmail.com>
2021-07-07 09:42:20 +0700

    feature login register logout forget password for user

--- resources/js/client/home.js
+++ resources/js/client/home.js
@@ -17,4 +17,91 @@ $(document).ready(function () {
             }
         })
     });
+
+    // login
+    $('#loginForm').on('submit', function (e) {
+        e.preventDefault();
+        let formData = $(this).serializeArray();
+        $(".invalid-feedback").children("strong").text("");
+        $("#loginForm input").removeClass("is-invalid");
+        $.ajax({
+            method: "POST",
+            headers: {
+                Accept: "application/json"
+            },
+            url: '/login',
+            data: formData,
+            success: () => window.location.reload(),
+            error: (response) => {
+                $("#loginForm #passwordInput").val('');
+                if (response.status === 422) {
+                    let errors = response.responseJSON.errors;
+                    Object.keys(errors).forEach(function (key) {
+                        $("#" + key + "Input").addClass("is-invalid");
+                        $("#" + key + "Error").children("strong").text(errors[key][0]);
+                    });
+                } else {
+                    window.location.reload();
+                }
+            }
+        })
+    });
+
+    //register
+    $('#registerForm').on('submit', function (e) {
+        e.preventDefault();
+        let formData = $(this).serializeArray();
+        $(".invalid-feedback").children("strong").text("");
+        $("#registerForm input").removeClass("is-invalid");
+        $.ajax({
+            method: "POST",
+            headers: {
+                Accept: "application/json"
+            },
+            url: '/register',
+            data: formData,
+            success: () => window.location.reload(),
+            error: (response) => {
+                if (response.status === 422) {
+                    let errors = response.responseJSON.errors;
+                    Object.keys(errors).forEach(function (key) {
+                        $("#" + key + "Input").addClass("is-invalid");
+                        $("#" + key + "Error").children("strong").text(errors[key][0]);
+                    });
+                } else {
+                    window.location.reload();
+                }
+            }
+        })
+    });
+
+    //forgot password
+    $('#forgotPassForm').on('submit', function (e) {
+        e.preventDefault();
+        let formData = $(this).serializeArray();
+        $(".invalid-feedback").children("strong").text("");
+        $("#forgotPassForm input").removeClass("is-invalid");
+        $.ajax({
+            method: "POST",
+            headers: {
+                Accept: "application/json"
+            },
+            url: '/password/email',
+            data: formData,
+            success: function (data) {
+                $('#message').children("strong").text(data.message)
+            },
+            error: (response) => {
+                if (response.status === 422) {
+                    let errors = response.responseJSON.errors;
+                    Object.keys(errors).forEach(function (key) {
+                        $("#" + key + "InputForgot").addClass("is-invalid");
+                        $("#" + key + "ErrorForgot").children("strong").text(errors[key][0]);
+                    });
+                } else {
+                    window.location.reload();
+                }
+            }
+        })
+    });
 });
