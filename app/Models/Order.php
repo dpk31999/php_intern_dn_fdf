2d75e9901334b85096bd881682c4178d76847651
Dong Huynh <d01295306466@gmail.com>
2021-07-18 20:12:51 +0700

    manage order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,7 +14,7 @@ class Order extends Model
         'status',
     ];
 
-    public function users()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
---
+++ resources/lang/vi/order.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'orders' => 'Danh sách đơn hàng',
+    'done' => 'Done',
+    'pending' => 'Pending',
+    'cancel' => 'Cancel',
+
+    'update_order_success' => 'Cập nhật đơn hàng thành công',
+    'no_access' => 'Bạn không thể thay đổi đơn hàng này',
+
+];
